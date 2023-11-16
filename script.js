804b9acaeeea5901c75f21c4ce45f8f03badefc6
vincentanu04 <vincentanu04@gmail.com>
2023-11-17 04:09:17 +0800

    Implement background image based on weather condition

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+const UNSPLASH_API_KEY = "_c3NxG1sZOA1HfpR32_bFcYuAsVeEqUoXQmPEF782vs";
+
 async function fetchWeather(location) {
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=99957c311f1042c0bdf83440231611&q=${location}`);
     const json = await response.json();
-    console.log(json);
     const parsedJSON = parseJSON(json);
-    console.log(parsedJSON);
+    displayData(parsedJSON);
 };
 
 function parseJSON(json) {
@@ -28,17 +29,28 @@ function parseLocation(json) {
 };
 
 function parseCurrent(json) {
-    const current = json.current
-    const temp_c = current.temp_c
-    const temp_f = current.temp_f
+    const current = json.current;
+    const temp_c = current.temp_c;
+    const temp_f = current.temp_f;
 
-    const condition = current.condition
-    const description = condition.text
+    const condition = current.condition;
+    const description = condition.text;
     return {
         temp_c,
         temp_f,
         description
-    }
+    };
+};
+
+async function displayData(json) {
+    const descriptionPhotoUrl = await getPhotoUrl(json.description);
+    document.body.style.backgroundImage = `url(${descriptionPhotoUrl})`;
+};
+
+async function getPhotoUrl(description) {
+    const response = await fetch(`https://api.unsplash.com/search/photos?client_id=${UNSPLASH_API_KEY}&orientation=landscape&page=1&per_page=1&query=${description}`);
+    const json = await response.json();
+    return json.results[0].urls.full;
 };
 
 fetchWeather('poland')
